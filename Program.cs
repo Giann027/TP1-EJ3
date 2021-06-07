deef5a5a162dd05914335b155d4e0751d3f6cb86
ricot <ricot@DESKTOP-FTHKA14>
2021-06-07 17:10:50 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TP1_EJ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = " Transformar de grados sexagesimales a radianes";
+
+            double constante, sexagesimales, radianes;
+
+            Console.Write("Ingrese el valor sexagesimal: ");
+            sexagesimales = Convert.ToSingle(Console.ReadLine());
+
+            constante = 0.0174532925;
+            radianes= sexagesimales*constante;
+
+            Console.Write($"el valor en radianes es {radianes}");
+        }
+    }
+}
